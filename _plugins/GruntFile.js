41ec54c0ddf0c181916c9fc3e7e11a19dd3a06c5
qinfenglin <qfl2000@gmail.com>
2017-01-03 10:19:42 +0800

    base DIYTheme from https://github.com/pigerla/pigerla.github.com

---
+++ _plugins/GruntFile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            options: {
+                keepSpecialComments: 0
+            },
+//压缩为*.min.css文件
+            minify: {
+                expand: true,
+                cwd: '../assets/themes/DIYtheme/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: '../assets/themes/DIYtheme/css/',
+                ext: '.min.css'
+            },
+//合并css
+            combineCSS: {
+                files: {
+                    '../assets/css/combine.min.css': [
+                        '../assets/css/animation.css',
+                        '../assets/themes/DIYtheme/css/syntax.css',
+                        '../assets/themes/DIYtheme/css/screen.css',
+                        '../assets/themes/DIYtheme/css/page.css',
+                        '../assets/css/media-query.css',
+                        '../assets/css/base.css',
+                        '../assets/css/common.css'
+
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            combineJS: {
+                files: {
+                    '../assets/js/combine.min.js': ['../assets/js/search-xml.js' , '../assets/js/backToTop.js']
+                }
+            }
+        }
+    });
+
+//加载 cssmin 插件
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+//加载 uglify 插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+// 默认被执行的任务列表。
+    grunt.registerTask('default', ['cssmin','uglify']);
+};
